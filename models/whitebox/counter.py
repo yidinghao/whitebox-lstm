8577af26b7c9e75a9993eefe4db61f627bbde0ad
Yiding Hao <yiding.hao@yale.edu>
2020-10-10 18:05:18 -0400

    Changed m2 to u

--- models/whitebox/counter.py
+++ models/whitebox/counter.py
@@ -18,7 +18,7 @@ class CounterRNN(WhiteBoxRNN):
         :param m: The constant used to saturate the gates
         :param u: The value used to increment the counter
         """
-        self.m2 = m if u is None else u
+        self.u = m if u is None else u
         dataset = load_counter()
         super(CounterRNN, self).__init__(dataset, 2, m=m)
 
@@ -27,8 +27,8 @@ class CounterRNN(WhiteBoxRNN):
         wch = np.zeros((self._hidden_size, self._hidden_size))
         bc = np.zeros(self._hidden_size)
         wcx = np.zeros((self._hidden_size, len(self._x_stoi)))
-        wcx[0, self._x_stoi["a"]] = self.m2
-        wcx[0, self._x_stoi["b"]] = -self.m2
+        wcx[0, self._x_stoi["a"]] = self.u
+        wcx[0, self._x_stoi["b"]] = -self.u
 
         return wch, wcx, bc
 
@@ -38,6 +38,6 @@ class CounterRNN(WhiteBoxRNN):
         w[self._y_stoi["True"], 0] = 1.
 
         b = np.zeros((len(self._y_stoi)))
-        b[self._y_stoi["False"]] = np.tanh(np.tanh(self.m2)) / 2
+        b[self._y_stoi["False"]] = np.tanh(np.tanh(self.u)) / 2
 
         return w, b
