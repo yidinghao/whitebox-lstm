c1d6804b6d5dc66c6d502fc9002100856e13ac4e
Yiding Hao <yiding.hao@yale.edu>
2020-10-21 12:08:53 -0400

    Condensed docstrings

--- attribution/src/heatmap.py
+++ attribution/src/heatmap.py
@@ -17,11 +17,8 @@ def _rescale_score_by_abs(score: float, max_score: float,
     score of 0. is mapped to 0.5.
 
     :param score: An attribution score
-
     :param max_score: The maximum possible attribution score
-
     :param min_score: The minimum possible attribution score
-
     :return: The normalized score
     """
     if -1e-5 < min_score and max_score < 1e-5:
@@ -40,7 +37,6 @@ def _get_rgb(c_tuple: Tuple[float]) -> str:
     Converts a color from a tuple with values in [0., 1.] to RGB format.
 
     :param c_tuple: A color
-
     :return: The color, in RGB format
     """
     return "#%02x%02x%02x" % tuple(int(i * 255.) for i in c_tuple[:3])
@@ -53,76 +49,64 @@ def _span_word(tag: Callable, text: Callable, word: str, score: float,
     color representing its attribution score.
 
     :param tag: The tag() method from yattag
-
     :param text: The text() method from yattag
-
     :param word: A word
-
-    :param score: The word's relevance score
-
+    :param score: The word's attribution score
     :param colormap: A matplotlib colormap
-
     :return: None
     """
     bg = colormap(score)
-    # fg = tuple(1. - c for c in bg)
-    # style = "background-color:" + _get_rgb(bg) + ";color:" + _get_rgb(fg)
-    style = "color:" + _get_rgb(bg) + ";font-weight:bold;"
+    style = "color:" + _get_rgb(bg) + ";font-weight:bold;background-color: " \
+                                      "#ffffff"
     with tag("span", style=style):
         text(" " + word + " ")
     text(" ")
 
 
-def html_heatmap(words: List[str], scores: List[float],
+def html_heatmap(tokens: List[str], scores: List[float],
                  cmap_name: str = "coolwarm") -> str:
     """
     Constructs a word-level heatmap in HTML format.
 
-    :param words: A sequence of words
-
-    :param scores: The attribution score assigned to each word
-
+    :param tokens: A sequence of tokens
+    :param scores: The attribution score assigned to each token
     :param cmap_name: A matplotlib diverging colormap
-
     :return: The heatmap, as HTML code
     """
     colormap = plt.get_cmap(cmap_name)
 
-    assert len(words) == len(scores)
+    assert len(tokens) == len(scores)
     max_s = max(scores)
     min_s = min(scores)
 
     doc, tag, text = Doc().tagtext()
 
-    for idx, w in enumerate(words):
+    for idx, w in enumerate(tokens):
         score = _rescale_score_by_abs(scores[idx], max_s, min_s)
         _span_word(tag, text, w, score, colormap)
 
     return doc.getvalue()
 
 
-def latex_heatmap(words: List[str], scores: List[float],
+def latex_heatmap(tokens: List[str], scores: List[float],
                   cmap_name: str = "coolwarm") -> str:
     """
         Constructs a word-level heatmap in LaTeX format.
 
-        :param words: A sequence of words
-
-        :param scores: The attribution score assigned to each word
-
+        :param tokens: A sequence of words
+        :param scores: The attribution score assigned to each token
         :param cmap_name: A matplotlib diverging colormap
-
         :return: The heatmap, as LaTeX code
         """
     colormap = plt.get_cmap(cmap_name)
 
-    assert len(words) == len(scores)
+    assert len(tokens) == len(scores)
     max_s = max(scores)
     min_s = min(scores)
 
     code = ""
     code_template = "\\textcolor[rgb]{{{},{},{}}}{{\\textbf{{{}}}}} "
-    for idx, w in enumerate(words):
+    for idx, w in enumerate(tokens):
         score = _rescale_score_by_abs(scores[idx], max_s, min_s)
         r, g, b, _ = colormap(score)
         code += code_template.format(r, g, b, w)
